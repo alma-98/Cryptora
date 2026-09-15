a4840e2e7d6213e0885d429189dd15a58c7a20cf
Alma <125168465+alma-98@users.noreply.github.com>
2026-09-15 22:49:06 +0700

    Add Cryptora backend application

---
+++ cryptora-backend/src/main/java/com/cryptora/CryptoraApplication.java
@@ -0,0 +1,12 @@
+package com.cryptora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoraApplication.class, args);
+    }
+}
